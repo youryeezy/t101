9d931d726f20b3ae404637267357c020dc04ab2a
youryeezy <youryeezy@github.com>
2022-10-10 23:35:17 +0300

    upd lr1

--- lr1/lr1_main.py
+++ lr1/lr1_main.py
@@ -129,6 +129,35 @@ def results(facts, rules):
                                                     'then': i['then']})
                             facts.append(i['then'])
                             fact.add(i['then'])
+            if j == 'not':
+                count = len(i['if'][j])
+                counter = 0
+                for s in i['if'][j]:
+                    if s not in fact:
+                        counter = counter + 1
+                    else:
+                        break
+                if counter == count:
+                    # interim_results.append([facts,i['then']])
+                    if len(transit_results) == 0:
+                        fac = facts.copy()
+                        transit_results.append({'if': fac, 'not': i['if'][j], 'then': i['then']})
+                        facts.append(i['then'])
+                        fact.add(i['then'])
+                    else:
+                        put = True
+                        for ress in transit_results:
+                            if 'not' in ress:
+                                if (ress['not'] == i['if'][j] and ress['then'] != i['then']) or (
+                                        ress['not'] != i['if'][j] and ress['then'] == i['then']):
+                                    put = False
+                                    break
+                        if put is True:
+                            fac = facts.copy()
+                            transit_results.append({'if': fac, 'not': i['if'][j], 'then': i['then']})
+                            facts.append(i['then'])
+                            fact.add(i['then'])
+
     return transit_results
 
 
@@ -138,22 +167,22 @@ def results(facts, rules):
 
 
 if '__main__' == __name__:
-    print(generate_simple_rules(100, 4, 10))
+
+
+    #print(generate_simple_rules(100, 4, 10000))
 
 
     #generate rules and facts and check time
     time_start = time()
-    N = 100000
-    M = 1000
-    rules = generate_simple_rules(100, 4, N)
-    facts = generate_rand_facts(100, M)
-    print("%d rules generated in %f seconds" % (N, time()-time_start))
+
+    rules = generate_simple_rules(100, 4, 10000)
+    facts = generate_rand_facts(100, 1000)
+    print("%d rules generated in %f seconds" % (1000, time()-time_start))
 
     time_start = time()
+    result = results(facts, rules)
+    time_result = time() - time_start
+    print(time_result)
 
-    #time_result = time() - time_start
-    #print(res)
-    # YOUR CODE HERE
-
-    #print("%d facts validated vs %d rules in %f seconds" % (M, N, time_result))
+    print("%d facts validated for %d rules in %f seconds" % (1000, 10000, time_result))
 
